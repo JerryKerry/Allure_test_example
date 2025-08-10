db92220101fb26962e37b5e8710a8bc261de2d51
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-08-10 14:24:25 +0300

    init

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class SelenideTest {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+
+        open("https://github.com");
+
+        Configuration.browserSize = "1920x1080";
+        $(".search-input-container").click();
+        $( "#query-builder-test").sendKeys("JerryKerry/Working_with_files");
+        $( "#query-builder-test").submit();
+        $(linkText("JerryKerry/Working_with_files")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
+
+    }
 }
--- src/test/java/WebSteps.java
+++ src/test/java/WebSteps.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class WebSteps {
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repositoryName}")
+    public void searchForRepository(String repositoryName){
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys(repositoryName);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория")
+    public void clickOneRepository(String repositoryName){
+        $(linkText(repositoryName)).click();
+    }
+
+    @Step("Открываем таб Issues")
+    public void openIssuesTab(){
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с номером")
+    public void shouldSeeIssueWithNumber(int issueNumber){
+        $(withText("#" + issueNumber)).should(Condition.exist);
+    }
+
 }
