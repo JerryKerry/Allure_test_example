3b9390e653476e2e88c38aff1ee2f81c0cf29da6
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-08-10 14:36:29 +0300

    убрал пустые строки

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
-
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.linkText;
@@ -14,10 +13,7 @@ public class SelenideTest {
     @Test
     public void testIssueSearch(){
         SelenideLogger.addListener("allure", new AllureSelenide());
-
-
         open("https://github.com");
-
         Configuration.browserSize = "1920x1080";
         $(".search-input-container").click();
         $( "#query-builder-test").sendKeys("JerryKerry/Working_with_files");
