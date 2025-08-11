5d425aac73c9ae43fde84b431adf3a290f5c93a8
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-08-11 21:56:44 +0300

    поправил Allure Listener

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -10,11 +11,17 @@ import static org.openqa.selenium.By.linkText;
 
 public class SelenideTest {
 
+    @BeforeEach
+    public void beforeEach() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     public void testIssueSearch(){
         SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com");
-        Configuration.browserSize = "1920x1080";
+
         $(".search-input-container").click();
         $( "#query-builder-test").sendKeys("JerryKerry/Working_with_files");
         $( "#query-builder-test").submit();
